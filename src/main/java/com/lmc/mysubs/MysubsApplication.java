1b4b8644bb0466d0b42e0b6c286ea938865f4a39
llucmarti <lluc.marti.calveres@gmail.com>
2024-12-14 16:48:27 +0100

    Initial commit

---
+++ src/main/java/com/lmc/mysubs/MysubsApplication.java
@@ -0,0 +1,13 @@
+package com.lmc.mysubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysubsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmc/mysubs/MysubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmc.mysubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
